6f2e28e31de56a54301900979b30352a8527724d
yagna789 <96456516+yagna789@users.noreply.github.com>
2021-12-21 08:24:02 +0530

    Add files via upload

---
+++ opoerator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class opoerator()
+{
+public static void main()
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("enter a numaaaaaaaaa');
+int w=sc.nextInt()
+if(w%2==0)
+{system.out.print("it is a multiple of 2"); 
+}
+else(w%2>0)
+{system.out.print("the no is not divisible by 2");
+}
+}
+}
